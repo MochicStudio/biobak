076b37226d8e5b90fa7df640831ac886018af206
Andres Ruiz <andrsruiz@gmail.com>
2020-11-16 00:27:43 -0600

    Spawn reverse stopwatch on game start

--- main.lua
+++ main.lua
@@ -3,11 +3,12 @@ local mod = RegisterMod("biobak-ardc", 1)
 -- Set item ENUM
 mod.COLLECTIBLE_RISK = Isaac.GetItemIdByName("Risk")
 mod.COLLECTIBLE_JADED_RING = Isaac.GetItemIdByName("Jaded Ring")
+mod.COLLECTIBLE_REVERSE_STOPWATCH = Isaac.GetItemIdByName("Reverse Stopwatch")
 
 function mod:PassiveItemRisk()
 	-- Beginning of game, initialization
 	if Game():GetFrameCount() == 1 then
-		Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, mod.COLLECTIBLE_RISK, Vector(320, 280), Vector(0, 0), nil)
+		Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, mod.COLLECTIBLE_RISK, Vector(150, 200), Vector(0, 0), nil)
 	end
 end
 
@@ -18,6 +19,14 @@ function mod:PassiveItemJadedRing()
 	end
 end
 
+function mod:PassiveItemReverseStopwatch()
+	-- Beginning of game, initialization
+	if Game():GetFrameCount() == 1 then
+		Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, mod.COLLECTIBLE_REVERSE_STOPWATCH, Vector(250, 200), Vector(0, 0), nil)
+	end
+end
+
 -- Callbacks
 mod:AddCallback(ModCallbacks.MC_POST_UPDATE, mod.PassiveItemRisk)
 mod:AddCallback(ModCallbacks.MC_POST_UPDATE, mod.PassiveItemJadedRing)
+mod:AddCallback(ModCallbacks.MC_POST_UPDATE, mod.PassiveItemReverseStopwatch)
