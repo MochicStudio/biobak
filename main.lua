f60fb03bcd78e8869ba5259e43cd6247e6bc6737
Andres Ruiz <andrsruiz@gmail.com>
2020-11-21 12:14:36 -0600

    UpdateInventory and SpawnItems local functions

--- main.lua
+++ main.lua
@@ -25,36 +25,22 @@ local ItemsBonus = {
 	JADED_RING_SHOT_SPEED = 2 -- Subtract shoot speed
 }
 
-function mod:PassiveItemRisk()
-	-- Beginning of game, initialization
-	if Game():GetFrameCount() == 1 then
+-- STARTS LOCAL FUNCTIONS
+-- Update Inventory
+local function UpdateInventory(player)
+	HasItem.risk = player:HasCollectible(ItemsId.RISK)
+	HasItem.jaded_ring = player:HasCollectible(ItemsId.JADED_RING)
+	HasItem.reverse_stopwatch = player:HasCollectible(ItemsId.REVERSE_STOPWATCH)
+	HasItem.magnifying_glass = player:HasCollectible(ItemsId.MAGNIFYING_GLASS)
+end
+
+-- Spawn Items, on game start. For test purposes.
+local function SpawnItems()
+	if game:GetFrameCount() == 1 then
 		Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, mod.COLLECTIBLE_RISK, Vector(150, 200), Vector(0, 0), nil)
-	end
-end
-
-function mod:PassiveItemJadedRing()
-	-- Beginning of game, initialization
-	if Game():GetFrameCount() == 1 then
 		Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, mod.COLLECTIBLE_JADED_RING, Vector(200, 200), Vector(0, 0), nil)
-	end
-end
-
-function mod:PassiveItemReverseStopwatch()
-	-- Beginning of game, initialization
-	if Game():GetFrameCount() == 1 then
 		Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, mod.COLLECTIBLE_REVERSE_STOPWATCH, Vector(250, 200), Vector(0, 0), nil)
-	end
-end
-
-function mod:PassiveItemMagnifyingGlass()
-	-- Beginning of game, initialization
-	if Game():GetFrameCount() == 1 then
 		Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, mod.COLLECTIBLE_MAGNIFYING_GLASS, Vector(300, 200), Vector(0, 0), nil)
 	end
 end
-
--- Callbacks
-mod:AddCallback(ModCallbacks.MC_POST_UPDATE, mod.PassiveItemRisk)
-mod:AddCallback(ModCallbacks.MC_POST_UPDATE, mod.PassiveItemJadedRing)
-mod:AddCallback(ModCallbacks.MC_POST_UPDATE, mod.PassiveItemReverseStopwatch)
-mod:AddCallback(ModCallbacks.MC_POST_UPDATE, mod.PassiveItemMagnifyingGlass)
+-- ENDS LOCAL FUNCTIONS
